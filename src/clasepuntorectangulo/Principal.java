c365a30cb2763c8b020ca5b8aa36ffece5a576e6
Jose Lopez <jlopezn5@gmail.com>
2020-03-10 23:12:39 +0100

    segunda

--- src/clasepuntorectangulo/Principal.java
+++ src/clasepuntorectangulo/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
         Punto p1 = new Punto(5, 5);
-        Punto p2 = new Punto(15, 20);
+        Punto p2 = new Punto(15, 25);
          p1.desplaza(5, 0);
         p2.desplaza(0, 3);
          Rectangulo r1 = new Rectangulo(p1, p2);
